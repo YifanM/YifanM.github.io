aef1000afa001899173489912a191964d0016172
Yifan Ma <yifan.ma@hotmail.com>
2016-05-05 04:59:22 -0400

    Added files via upload

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 var tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+//var onClick = false;
+var lastYear = -1, lastMonth = -1, lastDay = -1, lastHour = -1, lastMinute = -1;
 
 function GetClock(){
 	var d=new Date();
 	var nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getYear();
 	if(nyear<1000) nyear+=1900;
-
 	var nhour=d.getHours(),nmin=d.getMinutes();
-	if(nmin<=9) nmin="0"+nmin
+	if(nmin<=9) nmin="0"+nmin;
+/*
+	if (nyear != lastYear){
 
-		//document.getElementById('clock_date').innerHTML= "<span id='date'>tmonth[nmonth]+" "+ndate+", "+nyear</span>"; 
+	}
+	if (nmonth != lastMonth){
+
+	}
+	if (nday != lastDay){
+
+	}
+	if (nhour != lastHour){
+
+	}
+	if (nmin != lastMinute){
+
+	}*/
 	document.getElementById('clock_date').innerHTML= tmonth[nmonth]+" "+ndate+", "+nyear; 
-	    document.getElementById("clock_time").innerHTML= +nhour+":"+nmin+"";
+	document.getElementById("clock_time").innerHTML= +nhour+":"+nmin+"";
+	lastYear = nyear;
+	lastMonth = nmonth;
+	lastDay = nday;
+	lastHour = nhour;
+	lastMinute = nmin;
 }
 
 window.onload=function(){
 	GetClock();
 	setInterval(GetClock,1000);
+	//if (onCLick == true){
 	if (jQuery("body").is("#home_page")){ //make it so from clicking tab is fade, from other source is instant
 		jQuery(home_tab).addClass("active");
 	}
@@ -31,6 +52,8 @@ window.onload=function(){
 	else if (jQuery("body").is("#projects_page")){
 		jQuery(projects_tab).addClass("active");
 	}
+	//onClick = false;
+//}
 }
 
 
@@ -39,23 +62,24 @@ jQuery(document).ready(function() {
 		var currentAttrValue = jQuery(this).attr('href');
 		jQuery('.main_tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
 		jQuery(this).parent("li").addClass('active').siblings().removeClass('active');
-		switch(this.parentNode.id){
-			case "home_tab":
-			window.location.href = "index.html";
-			break;
-			case "me_tab":
-			window.location.href = "me.html";
-			break;
-			case "documents_tab":
-			window.location.href = "documents.html";
-			break;
-			case "uwaterloo_tab":
-			window.location.href = "uwaterloo.html";
-			break;
-			case "projects_tab":
-			window.location.href = "projects.html";
-			break;
-		}
-		e.preventDefault();
-	});
+	//	onClick = true;
+	e.preventDefault();
+	switch(this.parentNode.id){
+		case "home_tab":
+		window.location.href = "index.html";
+		break;
+		case "me_tab":
+		window.location.href = "me.html";
+		break;
+		case "documents_tab":
+		window.location.href = "documents.html";
+		break;
+		case "uwaterloo_tab":
+		window.location.href = "uwaterloo.html";
+		break;
+		case "projects_tab":
+		window.location.href = "projects.html";
+		break;
+	}
+});
 });

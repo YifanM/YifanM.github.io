1b12eee15fcce3af484a3c20a5ea6f996fa23408
Yifan Ma <yifan.ma@hotmail.com>
2016-05-10 00:05:14 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
 var tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
-//var onClick = false;
-var lastYear = -1, lastMonth = -1, lastDay = -1, lastHour = -1, lastMinute = -1;
+var d1, d2, curYear, curMonth, curDay, curHour, curMinute;
 
 function GetClock(){
-	var d=new Date();
-	var nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getYear();
-	if(nyear<1000) nyear+=1900;
-	var nhour=d.getHours(),nmin=d.getMinutes();
-	if(nmin<=9) nmin="0"+nmin;
-/*
-	if (nyear != lastYear){
-
+	d1=new Date();
+	curmonth=d1.getMonth(),curday=d1.getDate(),curyear=d1.getYear();
+	if(curyear<1000) curyear+=1900;
+	var curhour=d1.getHours(),curminute=d1.getMinutes();
+	if(curminute<=9) curminute="0"+curminute;
+	if (d2.getMinutes() != curminute ){
+		document.getElementById('clock_newTime').innerHTML= curhour+":"+curminute+""; 
+		//document.getElementById("clock_newTime").style.visibility="visible";
+		//document.getElementById("clock_time").fadeTo("slow", 0,00, function(){})
 	}
-	if (nmonth != lastMonth){
+	if (d2.getDate() != curday ){
+		document.getElementById('clock_newDate').innerHTML= tmonth[curmonth]+" "+curday+", "+curyear; 
+		//document.getElementById("clock_newDate").style.visibility="visible";
+     }
+	d2 = new Date(d1)
+}
 
-	}
-	if (nday != lastDay){
-
-	}
-	if (nhour != lastHour){
-
-	}
-	if (nmin != lastMinute){
-
-	}*/
-	document.getElementById('clock_date').innerHTML= tmonth[nmonth]+" "+ndate+", "+nyear; 
-	document.getElementById("clock_time").innerHTML= +nhour+":"+nmin+"";
-	/*lastYear = nyear;
-	lastMonth = nmonth;
-	lastDay = nday;
-	lastHour = nhour;
-	lastMinute = nmin;*/
+function getInitialClock(){
+d1=new Date();
+	curmonth=d1.getMonth(),curday=d1.getDate(),curyear=d1.getYear();
+	if(curyear<1000) curyear+=1900;
+	var curhour=d1.getHours(),curminute=d1.getMinutes();
+	if(curminute<=9) curminute="0"+curminute;
+	document.getElementById('clock_date').innerHTML= tmonth[curmonth]+" "+curday+", "+curyear; 
+	document.getElementById("clock_time").innerHTML= +curhour+":"+curminute+"";
 }
 
 function getActive(){
-		if (jQuery("body").is("#home_page")){ //make it so from clicking tab is fade, from other source is instant
+		if (jQuery("body").is("#home_page")){ 
 		jQuery(home_tab).addClass("active");
 	}
 	else if (jQuery("body").is("#me_page")){
@@ -52,6 +48,8 @@ function getActive(){
 }
 
 window.onload=function(){
+	d2 = new Date();
+	getInitialClock();
 	GetClock();
 	setInterval(GetClock,1000);
 	getActive();
@@ -64,7 +62,6 @@ jQuery(document).ready(function() {
 		var currentAttrValue = jQuery(this).attr('href');
 		jQuery('.main_tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
 		jQuery(this).parent("li").addClass('active').siblings().removeClass('active');
-	//	onClick = true;
 	e.preventDefault();
 	switch(this.parentNode.id){
 		case "home_tab":
